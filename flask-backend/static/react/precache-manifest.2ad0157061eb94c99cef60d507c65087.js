557574e4908e7528789d48ab0f4f4cbfc2cb3da3
Maarten <maartenhop@outlook.com>
2020-01-03 12:52:16 +0100

    react template working, but not in flask environment

---
+++ flask-backend/static/react/precache-manifest.2ad0157061eb94c99cef60d507c65087.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30848aebdfbf993952fb622a4f6371b",
+    "url": "/../../templates/index.html"
+  },
+  {
+    "revision": "3d1becf8003db38f7bd7",
+    "url": "/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4bd91a79c211752d9ae2",
+    "url": "/js/2.9b605f92.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/js/2.9b605f92.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d1becf8003db38f7bd7",
+    "url": "/js/main.291ae016.chunk.js"
+  },
+  {
+    "revision": "f13699b9e84d10749763",
+    "url": "/js/runtime-main.627809a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/media/logo.5d5d9eef.svg"
+  }
+]);
